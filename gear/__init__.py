f5021dcb852c20a19b9aee86b101e4ba1502d974
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-07 19:49:59 +0800

    gear mods

--- gear/__init__.py
+++ gear/__init__.py
@@ -1,23 +1,31 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-from gear.attribute import (CoreAttribute, CriticalHitDamage, MinorAttribute,
-                            RedCore)
+import gear.attribute as attr
+import gear.mods as mods
 
 
 @dataclass(kw_only=True)
 class Gear:
     name: str
-    core: CoreAttribute
-    attr1: MinorAttribute
+    core: attr.CoreAttribute
+    attr1: attr.MinorAttribute
+    mod: mods.Mod = field(default_factory=mods.NoMod)
 
     @property
     def weapon_damage_pct(self) -> float:
-        if isinstance(self.core, RedCore):
+        if isinstance(self.core, attr.RedCore):
             return self.core.weapon_damage_pct
         return 0.0
 
     @property
     def critical_hit_damage_pct(self) -> float:
-        if isinstance(self.attr1, CriticalHitDamage):
-            return self.attr1.pct
-        return 0.0
+        pct = 0
+        # attribute
+        if isinstance(self.attr1, attr.CriticalHitDamage):
+            pct += self.attr1.pct
+        # mods
+        if isinstance(self.mod, mods.CriticalHitDamage):
+            pct += self.mod.pct
+
+        # result
+        return pct
