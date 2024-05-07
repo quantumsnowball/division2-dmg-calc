12799828a4036903d9343d19a99c56c2ec2df03c
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-07 14:46:08 +0800

    idea

---
+++ idea.py
@@ -0,0 +1,12 @@
+"""
+legendary1 = Build(
+        weapon=StElmo()
+        mask=Striker(chc=0.06, chd=0.12),
+        backpack=Striker(chc=0.06, chd=0.12),
+        chest=Striker(chc=0.06, chd=0.12),
+        gloves=Striker(chc=0.06, chd=0.12),
+        holster=Striker(chc=0.06, chd=0.12),
+        kneepads=FoxPrayer(chc=0.06, chd=0.12),
+        specialization=Gunner()
+        )
+"""
