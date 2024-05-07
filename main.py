4c06d4801502765148fd7e25531c6af2e034d99f
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-07 21:32:15 +0800

    abbr

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pprint import pprint
 
+import gear.attribute as gearattr
+import gear.mods as gearmods
 from build import Build
 from build.specialization import Gunner
-from gear.attribute import CriticalHitChance
 from gear.FoxPrayer import FoxPrayer
 from gear.Lengmo import Lengmo
-from gear.mods import CriticalHitDamage
 from gear.Striker import Striker
 from weapon.StElmosEngine import StElmosEngine
 
@@ -18,13 +18,13 @@ def main():
         # weapons
         weapon=StElmosEngine(expertise_level=17),
         # gears
-        mask=Striker(attr1=CriticalHitChance(0.06),
-                     mod=CriticalHitDamage(0.12)),
-        backpack=Striker(mod=CriticalHitDamage(0.12)),
-        chest=Lengmo(mod=CriticalHitDamage(0.119)),
+        mask=Striker(attr1=gearattr.CHC(0.06),
+                     mod=gearmods.CHD(0.12)),
+        backpack=Striker(mod=gearmods.CHD(0.12)),
+        chest=Lengmo(mod=gearmods.CHD(0.119)),
         gloves=Striker(),
         holster=Striker(),
-        kneepads=FoxPrayer(attr1=CriticalHitChance(0.06)),
+        kneepads=FoxPrayer(attr1=gearattr.CHC(0.06)),
     )
 
     pprint(build)
