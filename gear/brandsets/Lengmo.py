762e144dab828cded40a0d721769da79f1f6ebfb
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-08 12:59:45 +0800

    subclassing Brandsets

--- gear/brandsets/Lengmo.py
+++ gear/brandsets/Lengmo.py
@@ -2,10 +2,11 @@ from dataclasses import dataclass, field
 
 import gear
 import gear.attrs as attrs
+from gear.brandsets import Brandsets
 
 
 @dataclass(kw_only=True)
-class Chest(gear.Chest):
+class Chest(gear.Chest, Brandsets):
     name: str = 'Lengmo'
     core: attrs.CoreAttribute = field(default_factory=attrs.BlueCore)
     attr1: attrs.MinorAttribute = field(default_factory=attrs.ExplosiveResistence)
--- gear/brandsets/OverlordArmaments.py
+++ gear/brandsets/OverlordArmaments.py
@@ -2,10 +2,11 @@ from dataclasses import dataclass, field
 
 import gear
 import gear.attrs as attrs
+from gear.brandsets import Brandsets
 
 
 @dataclass(kw_only=True)
-class FoxsPrayer(gear.Kneepads):
+class FoxsPrayer(gear.Kneepads, Brandsets):
     name: str = "Fox's Prayer"
     core: attrs.CoreAttribute = field(default_factory=attrs.RedCore)
     attr1: attrs.MinorAttribute = field(default_factory=attrs.DamageToTargetOutOfCover)
