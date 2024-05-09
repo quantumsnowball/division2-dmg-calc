b40299ebee33b58d08c410e8c2495c96b009da1a
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-09 21:11:35 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='division2calc',
+    version='0.1.0',
+    description='division2calc - a calculator for division 2',
+    url='https://github.com/quantumsnowball/division2calc',
+    author='Quantum Snowball',
+    author_email='quantum.snowball@gmail.com',
+    license='MIT',
+    packages=['division2calc'],
+    zip_safe=False,
+    # entry_points={
+    #     'console_scripts': [
+    #         'avdl=avdl.cli:avdl',
+    #     ]
+    # }
+)
