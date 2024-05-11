a0f80ac2764d7ce9b6c439c10e3ad8a23ea39a5d
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-11 16:30:54 +0800

    def load_builds_file

--- division2calc/__init__.py
+++ division2calc/__init__.py
@@ -12,8 +12,7 @@ import division2calc.gear.gearsets.StrikersBattlegear as Striker
 import division2calc.gear.mods as gearmods
 from division2calc.build import Build
 from division2calc.build.specialization import Gunner
-from division2calc.utils import (build_as_yaml, dataclass_asdict,
-                                 load_build_file, pformat_dataclass)
+from division2calc.utils import load_builds_file, pformat_dataclass
 from division2calc.weapon.StElmosEngine import StElmosEngine
 
 __all__ = [
@@ -35,14 +34,16 @@ def division2calc() -> None:
 
 @division2calc.command()
 @click.argument('file', required=True, type=click.Path())
+@click.option('-i', '--index', type=int, default=0, help='which build in the list to display')
 @click.option('--x', is_flag=True, default=False, help='Enable x summary')
 @click.option('--damage', is_flag=True, default=False, help='Enable damage summary')
 @click.option('--dydx', is_flag=True, default=False, help='Enable dydx summary')
 def summary(file: Path,
+            index: int,
             damage: bool,
             x: bool,
             dydx: bool) -> None:
-    build = load_build_file(file)
+    build = load_builds_file(file)[index]
     all = not any((x, damage, dydx))
     if all or damage:
         click.secho(f'\ndamage: Build({build.name})', fg='yellow')
