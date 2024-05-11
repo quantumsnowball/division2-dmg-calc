c09939e82dd6beae7a8648672a4889a2f75a823c
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-12 03:27:28 +0800

    dydx

---
+++ division2calc/command/dydx.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import click
+
+from division2calc.build.common import PROFILES
+from division2calc.command.utils import load_builds_metric
+
+
+@click.command()
+@click.argument('file', required=True, type=click.Path())
+def dydx(file: Path) -> None:
+    for profile in PROFILES:
+        # load
+        df = load_builds_metric(file, 'dydx', profile)
+        # format
+        df = df.map(lambda v: f'{v:.3f}')
+        # result
+        click.secho(f'\ndydx - profile `{profile}`:', fg='yellow')
+        print(df)
