68328be3220867489eb4a470bc76ac79053c3822
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-12 02:33:34 +0800

    compare

--- division2calc/__init__.py
+++ division2calc/__init__.py
@@ -13,6 +13,7 @@ import division2calc.gear.mods as gearmods
 from division2calc.build import Build
 from division2calc.build.common import Metric, Profile, SortBy, SortOrder
 from division2calc.build.specialization import Gunner
+from division2calc.command.compare import compare
 from division2calc.command.rank import rank
 from division2calc.command.stats import stats
 from division2calc.utils import load_builds_file, pformat_dataclass
@@ -72,36 +73,5 @@ def summary(file: Path,
     # print(pformat_dataclass(build))
 
 
-@division2calc.command()
-@click.argument('file', required=True, type=click.Path())
-@click.option('-i', '--indices', default=(0, 1,), type=click.Tuple([int, int]),
-              help='which two builds in the list to display, e.g. -i 0 1')
-@click.option('--x', is_flag=True, default=False, help='Enable x comparison')
-@click.option('--damage', is_flag=True, default=False, help='Enable damage comparison')
-@click.option('--dydx', is_flag=True, default=False, help='Enable dydx comparison')
-def compare(file: Path,
-            indices: tuple[int, int],
-            damage: bool,
-            x: bool,
-            dydx: bool) -> None:
-    builds = load_builds_file(file)
-    build1, build2 = tuple(builds[i] for i in indices)
-    all = not any((x, damage, dydx))
-    if all or damage:
-        click.secho(f'\ndiff(damage): Build({build2.name}) net Build({build1.name})', fg='yellow')
-        diff: pd.DataFrame = build2.summary.damage - build1.summary.damage
-        print(diff.round(2))
-        click.secho(f'\ndiff%(damage): Build({build2.name}) net Build({build1.name})', fg='yellow')
-        diffpct: pd.DataFrame = (build2.summary.damage / build1.summary.damage - 1)*100
-        print(diffpct.round(4))
-    if all or x:
-        click.secho(f'\ndiff(x): Build({build2.name}) net Build({build1.name})', fg='yellow')
-        diff: pd.DataFrame = build2.summary.x - build1.summary.x
-        print(diff.round(4))
-    if all or dydx:
-        click.secho(f'\ndiff(dydx): Build({build2.name}) net Build({build1.name})', fg='yellow')
-        diff: pd.DataFrame = build2.summary.dydx - build1.summary.dydx
-        print(diff.round(4))
-
-
+division2calc.add_command(compare)
 division2calc.add_command(rank)
