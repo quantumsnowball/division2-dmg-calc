4ae87edbe86d1728dab128cd95ca97969022be1e
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-12 04:05:36 +0800

    guns-battle

---
+++ sheets/guns-battle.py
@@ -0,0 +1,26 @@
+from dataclasses import replace
+
+from division2calc import *
+
+base = Build(
+    name='StElmosEngine',
+    # specialization
+    specialization=Gunner(),
+    # weapons
+    weapon=AR.StElmosEngine(expertise_level=15),
+    # gears
+    mask=Striker.Mask(attr1=gearattrs.CHC(0.06),
+                      mod=gearmods.CHD(0.12)),
+    backpack=Striker.Backpack(mod=gearmods.CHD(0.12)),
+    chest=Lengmo.Chest(mod=gearmods.CHD(0.119)),
+    gloves=Striker.Gloves(),
+    holster=Striker.Holster(),
+    kneepads=Overlord.FoxsPrayer(attr2=gearattrs.CHC(0.06)),
+)
+
+builds = [base, ]
+builds.append(replace(
+    builds[-1],
+    name='ShieldSplinterer',
+    weapon=AR.ShieldSplinterer(expertise_level=12),
+))
