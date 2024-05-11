ce583de34214e03f04254dca40991af798aca3bb
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-11 23:42:48 +0800

    rename build sheet

--- build_sheet.py
+++ build_sheet.py
@@ -30,5 +30,20 @@ builds = [
         gloves=Striker.Gloves(),
         holster=Striker.Holster(),
         kneepads=Overlord.FoxsPrayer(attr2=gearattrs.CHC(0.06)),
-    )
+    ),
+    Build(
+        name='Legendary3',
+        # specialization
+        specialization=Gunner(),
+        # weapons
+        weapon=StElmosEngine(expertise_level=15),
+        # gears
+        mask=Striker.Mask(attr1=gearattrs.CHD(0.12),
+                          mod=gearmods.CHD(0.12)),
+        backpack=Striker.Backpack(mod=gearmods.CHD(0.12)),
+        chest=Lengmo.Chest(mod=gearmods.CHD(0.12)),
+        gloves=Striker.Gloves(),
+        holster=Striker.Holster(),
+        kneepads=Overlord.FoxsPrayer(attr2=gearattrs.CHC(0.06)),
+    ),
 ]
