ce23ad99bdd074faa786ff5fb697152d44c0022f
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-13 05:11:40 +0800

    X7 Float

--- division2calc/build/damage/X/X7.py
+++ division2calc/build/damage/X/X7.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
+
 from division2calc.build.damage.common import Profile
 from division2calc.build.stats import Stats
+from division2calc.utils import Float
 
 
 @dataclass
-class X7(Profile[float]):
+class X7(Profile[Float]):
     _stats: Stats
 
-    def __call__(self, armor: bool) -> float:
-        x = 1.0
+    def __call__(self, armor: bool) -> Float:
+        x = Float(1.0)
         if armor:
             x += self._stats.damage_to_armor
         else:
@@ -17,29 +19,32 @@ class X7(Profile[float]):
         return x
 
     @property
-    def basic(self) -> float:
-        return 1.0
+    def basic(self) -> Float:
+        return Float(1.0)
 
     @property
-    def min(self) -> float:
-        x = 1.0
+    def min(self) -> Float:
+        x = Float(1.0)
         # assume whoever being the lowest
+        # TODO
         x += min(self._stats.damage_to_armor, self._stats.damage_to_health)
         # result
         return x
 
     @property
-    def average(self) -> float:
+    def average(self) -> Float:
         # assuming equal chance for armor and health damage
-        x = 1.0
+        x = Float(1.0)
+        # TODO
         x += 0.5*self._stats.damage_to_armor + 0.5*self._stats.damage_to_health
         # result
         return x
 
     @property
-    def max(self) -> float:
-        x = 1.0
+    def max(self) -> Float:
+        x = Float(1.0)
         # assume whoever being the highest
+        # TODO
         x += max(self._stats.damage_to_armor, self._stats.damage_to_health)
         # result
         return x
