c897f9186721d63987579a48e40599475533256b
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-13 05:10:13 +0800

    X6 Float

--- division2calc/build/damage/X/X6.py
+++ division2calc/build/damage/X/X6.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+
 from division2calc.build.damage.common import Profile
 from division2calc.build.stats import Stats
+from division2calc.utils import Float
 
 
 @dataclass
-class X6(Profile[float]):
+class X6(Profile[Float]):
     _stats: Stats
 
     def __call__(self, critical: bool, headshot: bool, expcrit: bool) -> float:
@@ -20,24 +22,25 @@ class X6(Profile[float]):
         return x
 
     @property
-    def basic(self) -> float:
-        return 1.0
+    def basic(self) -> Float:
+        return Float(1.0)
 
     @property
-    def min(self) -> float:
+    def min(self) -> Float:
         return self.basic
 
     @property
-    def average(self) -> float:
-        x = 1.0
+    def average(self) -> Float:
+        x = Float(1.0)
         # weight by critical hit chance
         x += self._stats.critical_hit_chance*self._stats.critical_hit_damage
+        # TODO
         # result
         return x
 
     @property
-    def max(self) -> float:
-        x = 1.0
+    def max(self) -> Float:
+        x = Float(1.0)
         # assuming critical headshot
         x += self._stats.critical_hit_damage
         x += self._stats.headshot_damage
