b52dc3ec78fdaf6766b2ec356cbf11afeefd3415
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-13 05:12:14 +0800

    X8 Float

--- division2calc/build/damage/X/X8.py
+++ division2calc/build/damage/X/X8.py
@@ -1,27 +1,29 @@
 from dataclasses import dataclass
+
 from division2calc.build.damage.common import Profile
 from division2calc.build.stats import Stats
+from division2calc.utils import Float
 
 
 @dataclass
-class X8(Profile[float]):
+class X8(Profile[Float]):
     _stats: Stats
 
     @property
-    def basic(self) -> float:
-        return 1.0
+    def basic(self) -> Float:
+        return Float(1.0)
 
     @property
-    def min(self) -> float:
-        x = 1.0
+    def min(self) -> Float:
+        x = Float(1.0)
         x += self._stats.damage_to_target_out_of_cover
         # result
         return x
 
     @property
-    def average(self) -> float:
+    def average(self) -> Float:
         return self.min
 
     @property
-    def max(self) -> float:
+    def max(self) -> Float:
         return self.min
