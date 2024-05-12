2d369a124319375b29eebc82441850bb1fafd054
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-12 18:57:58 +0800

    def test_compare_0(i1: int, i2: int, flag: str | None, script_runner: ScriptRunner):

--- tests/test_command.py
+++ tests/test_command.py
@@ -43,18 +43,20 @@ def test_summary_non0(i_: int, script_runner: ScriptRunner):
     assert r.returncode != 0
 
 
-def test_compare(script_runner: ScriptRunner):
+@pytest.mark.parametrize('i1,i2', ((1, 1), (1, 2), (-1, 1), (2, -2)))
+@pytest.mark.parametrize('flag', ('--x', '--damage', '--dydx', None))
+def test_compare_0(i1: int, i2: int, flag: str | None, script_runner: ScriptRunner):
+    parts = [CMD, 'compare', '-i', i1, i2, flag,  FILE]
+    r = script_runner.run([str(v) for v in parts if v is not None])
+    assert r.returncode == 0
+
+
+def test_compare_non0(script_runner: ScriptRunner):
     r = script_runner.run([CMD, 'compare', '-i', '0', FILE])
     assert r.returncode != 0
-    for i1, i2 in [(1, 1), (1, 2), (-1, 1), (2, -2)]:
-        r = script_runner.run([CMD, 'compare', '-i', str(i1), str(i2),  FILE])
-        assert r.returncode == 0
     for i1, i2 in [('a', 1), (1, 'b'), (-999, 1), (999, -2)]:
         r = script_runner.run([CMD, 'compare', '-i', str(i1), str(i2),  FILE])
         assert r.returncode != 0
-    for flag in ['x', 'damage', 'dydx']:
-        r = script_runner.run([CMD, 'compare', '-i', '1', '2', f'--{flag}',  FILE])
-        assert r.returncode == 0
     for flag in ['abc', '123', 'anything']:
         r = script_runner.run([CMD, 'compare', '-i', '1', '2', f'--{flag}',  FILE])
         assert r.returncode != 0
