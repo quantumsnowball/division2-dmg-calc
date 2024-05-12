a6d83d46916206b14008fc9a2508b8e8e9c1a7fe
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-13 04:54:09 +0800

    stats Float all

--- division2calc/build/stats.py
+++ division2calc/build/stats.py
@@ -16,7 +16,7 @@ class Stats:
 
     @property
     def weapon_damage(self) -> Float:
-        pct = 0
+        pct = Float(0)
         # gear core attributes
         for gear in self._gears:
             pct += gear.weapon_damage
@@ -25,22 +25,22 @@ class Stats:
         # expertise level
         pct += self._weapon.weapon_damage
         # result
-        return Float(pct)
+        return pct
 
     @property
     def weapon_type_damage(self) -> Float:
-        pct = 0
+        pct = Float(0)
         # weapon attributes
         pct += self._weapon.weapon_type_damage
         # specialization bonus
         pct += self._specialization.weapon_type_damage(self._weapon.type)
         # result
-        return Float(pct)
+        return pct
 
     @property
     def critical_hit_chance(self) -> Float:
         # base CHC
-        pct = 0.0
+        pct = Float(0.0)
         # weapon
         pct += self._weapon.critical_hit_chance
         # gear
@@ -49,14 +49,14 @@ class Stats:
         # keener's watch
         pct += self._watch.critical_hit_chance
         # hard cap to 60%
-        pct = min(0.6, pct)
+        pct = Float(min(0.6, pct), pct.src)
         # result
-        return Float(pct)
+        return pct
 
     @property
     def critical_hit_damage(self) -> Float:
         # base CHD
-        pct = 0.25
+        pct = Float(0.25)
         # weapon
         pct += self._weapon.critical_hit_damage
         # gear
@@ -65,12 +65,12 @@ class Stats:
         # keener's watch
         pct += self._watch.critical_hit_damage
         # result
-        return Float(pct)
+        return pct
 
     @property
     def headshot_damage(self) -> Float:
         # base HS
-        pct = 0.55
+        pct = Float(0.55)
         # weapon
         pct += self._weapon.headshot_damage
         # gear
@@ -79,36 +79,36 @@ class Stats:
         # keener's watch
         pct += self._watch.headshot_damage
         # result
-        return Float(pct)
+        return pct
 
     @property
     def damage_to_armor(self) -> Float:
-        pct = 0
+        pct = Float(0)
         # weapon
         # TODO
         # gear
         # TODO
         # result
-        return Float(pct)
+        return pct
 
     @property
     def damage_to_health(self) -> Float:
-        pct = 0
+        pct = Float(0)
         # weapon
         pct += self._weapon.damage_to_health
         # gear
         for gear in self._gears:
             pct += gear.damage_to_health
         # result
-        return Float(pct)
+        return pct
 
     @property
     def damage_to_target_out_of_cover(self) -> Float:
-        pct = 0
+        pct = Float(0)
         # weapon
         pct += self._weapon.damage_to_target_out_of_cover
         # gear
         for gear in self._gears:
             pct += gear.damage_to_target_out_of_cover
         # result
-        return Float(pct)
+        return pct
