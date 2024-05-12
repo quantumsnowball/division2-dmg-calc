22aa95d791cb1ad2dc3e12aba11389dd14e31f40
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-13 04:59:12 +0800

    X1 Float

--- division2calc/build/damage/X/X1.py
+++ division2calc/build/damage/X/X1.py
@@ -1,24 +1,29 @@
 from dataclasses import dataclass
+
 from division2calc.build.damage.common import Profile
 from division2calc.build.stats import Stats
+from division2calc.utils import Float
 
 
 @dataclass
-class X1(Profile[float]):
+class X1(Profile[Float]):
     _stats: Stats
 
     @property
-    def basic(self) -> float:
-        return 1+self._stats.weapon_damage+self._stats.weapon_type_damage
+    def basic(self) -> Float:
+        pct = Float(1.0)
+        pct += self._stats.weapon_damage
+        pct += self._stats.weapon_type_damage
+        return pct
 
     @property
-    def min(self) -> float:
+    def min(self) -> Float:
         return self.basic
 
     @property
-    def average(self) -> float:
+    def average(self) -> Float:
         return self.basic
 
     @property
-    def max(self) -> float:
+    def max(self) -> Float:
         return self.basic
