227205ae2c23c02783fdddf2d789a8ea8bc4a869
Quantum Snowball <quantum.snowball@gmail.com>
2024-05-13 15:57:28 +0800

    Watch sourced

--- division2calc/build/watch.py
+++ division2calc/build/watch.py
@@ -14,17 +14,26 @@ class Watch:
     def weapon_damage(self) -> Float:
         unit = 0.002
         pct = self.weapon_damage_score * unit
-        src = [f'{self.__class__.__name__}({pct})', ]
+        src = [f'{self.__class__.__name__}.WD({pct})', ]
         return Float(pct, src)
 
     @property
     def critical_hit_chance(self) -> Float:
-        return Float(self.critical_hit_chance_score * 0.002)
+        unit = 0.002
+        pct = self.critical_hit_chance_score * unit
+        src = [f'{self.__class__.__name__}.CHC({pct})', ]
+        return Float(pct, src)
 
     @property
     def critical_hit_damage(self) -> Float:
-        return Float(self.critical_hit_damage_score * 0.004)
+        unit = 0.004
+        pct = self.critical_hit_damage_score * unit
+        src = [f'{self.__class__.__name__}.CHD({pct})', ]
+        return Float(pct, src)
 
     @property
     def headshot_damage(self) -> Float:
-        return Float(self.headshot_damage_score * 0.004)
+        unit = 0.004
+        pct = self.headshot_damage_score * unit
+        src = [f'{self.__class__.__name__}.HS({pct})', ]
+        return Float(pct, src)
